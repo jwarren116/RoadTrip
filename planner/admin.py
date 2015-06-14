3ce92874933320fa66079fd726a5fec58ef4dbef
James Warren <jwarren116@gmail.com>
2015-06-14 14:12:34 -0700

    Add TripDetailAdmin to Admin interface; register additional models with Admin interface

--- planner/admin.py
+++ planner/admin.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from planner.models import Route, Waypoint, RoadTrip, TripDetail
 
 
-class RoadTripAdmin(admin.ModelAdmin):
-    model = RoadTrip
-    list_display = ('route', 'start_date', 'end_date',)
-
-
 class WaypointInline(admin.StackedInline):
     model = Waypoint
     extra = 1
@@ -16,8 +11,19 @@ class WaypointInline(admin.StackedInline):
 class RouteAdmin(admin.ModelAdmin):
     model = Route
     inlines = [WaypointInline]
-    list_display = ('origin', 'destination',)
 
 
-# admin.site.register(Route, RouteAdmin)
+class RoadTripAdmin(admin.ModelAdmin):
+    model = RoadTrip
+    list_display = ('route', 'start_date', 'end_date',)
+
+
+class TripDetailAdmin(admin.ModelAdmin):
+    model = TripDetail
+    list_display = ('destination',)
+
+
+admin.site.register(Route, RouteAdmin)
+admin.site.register(Waypoint)
+admin.site.register(TripDetail)
 admin.site.register(RoadTrip, RoadTripAdmin)
