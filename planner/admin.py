f548db48abe1dcbead7283d0a01020910bd36210
James Warren <jwarren116@gmail.com>
2015-06-14 09:56:38 -0700

    Add rough layout for Admin interface; register planner app in settings

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from planner.models import Route, Waypoint, RoadTrip, TripDetail
+
+
+class RoadTripAdmin(admin.ModelAdmin):
+    model = RoadTrip
+    list_display = ('start_date', 'end_date', 'route',)
+
+
+class WaypointInline(admin.StackedInline):
+    model = Waypoint
+    extra = 1
+
+
+class RouteAdmin(admin.ModelAdmin):
+    model = Route
+    inlines = [WaypointInline]
+    list_display = ('origin', 'destination',)
+
+
+admin.site.register(Route, RouteAdmin)
+admin.site.register(RoadTrip, RoadTripAdmin)
