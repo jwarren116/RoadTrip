cab3cca494ef81139bba51ae00e9145b08a80728
James Warren <jwarren116@gmail.com>
2015-06-17 14:30:41 -0700

    Add static file root and directories to settings, and removed extra whitespace

--- RoadTrip/settings.py
+++ RoadTrip/settings.py
@@ -68,21 +68,17 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = BASE_DIR + '/public/static/'
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'RoadTrip/static/'),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'planner/templates/planner/'),
