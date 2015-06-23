284b9d931cea4e4048daa0655fede5929e9f12eb
James Warren <jwarren116@gmail.com>
2015-06-23 12:25:44 -0700

    Rework findPlaces and findPlacesByText functions to deal with no input appropriately.
    - Change how findPlacesByText selects values; prior method ended up with querySelector returning null
    - findPlacesByText now builds a string from values of selected elements
    - findPlaces and findPlacesByText now only search if length of selectedTypes is greater than 0

--- RoadTrip/static/js/script.js
+++ RoadTrip/static/js/script.js
@@ -79,38 +79,50 @@ function findPlaces(boxes, searchIndex) {
     types: selectedTypes
   };
 
-  service.radarSearch(request, function (results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
+  if (selectedTypes.length > 0) {
+    service.radarSearch(request, function (results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
       }
-    }
-    searchIndex++;
-    if (searchIndex < boxes.length) {
-      findPlaces(boxes, searchIndex);
-    }
-  });
+      searchIndex++;
+      if (searchIndex < boxes.length) {
+        findPlaces(boxes, searchIndex);
+      }
+    });
+  }
 }
 
 function findPlacesByText(boxes, searchIndex) {
-  var selectedTypes = document.querySelector('.textOption:checked').value;
+  var selectedTypes = ''; 
+
+  var inputElements = document.getElementsByClassName('textOption');
+  
+  for (var i=0; inputElements[i]; ++i) {
+    if (inputElements[i].checked) {
+     selectedTypes += inputElements[i].value + ', '
+    }
+  }
 
   var request = {
     bounds: boxes[searchIndex],
     query: selectedTypes
   };
 
-  service.textSearch(request, function (results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
+  if (selectedTypes.length > 0) {
+    service.textSearch(request, function (results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
       }
-    }
-    searchIndex++;
-    if (searchIndex < boxes.length) {
-      findPlacesByText(boxes, searchIndex);
-    }
-  });
+      searchIndex++;
+      if (searchIndex < boxes.length) {
+        findPlacesByText(boxes, searchIndex);
+      }
+    });
+  }
 }
 
 function createMarker(place) {
